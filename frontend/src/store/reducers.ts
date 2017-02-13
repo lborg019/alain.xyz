75db7e95bb46ee727236f4e4906060478b86e2c2
Alain Galvan <hi@alain.xyz>
2017-02-12 19:20:35 -0500

    Fixed sorting for dates

--- frontend/src/store/reducers.ts
+++ frontend/src/store/reducers.ts
@@ -22,7 +22,7 @@ let cachePortfoliosubapps = (subapps: APIResponse[], portfolio: APIResponse[]) =
             if (subapp['data'] !== undefined)
                 cache[index] = subapp;
         } else {
-            cache = [...cache, subapp].sort((a, b) => new Date(a.publishDate).getTime() - new Date(b.publishDate).getTime());
+            cache = [...cache, subapp].sort((a, b) => -(new Date(a.publishDate).getTime()) + new Date(b.publishDate).getTime());
         }
     }
 
