9031bce093034399bbedb18296a33f1dcb5b3c8f
Alain Galvan <hi@alain.xyz>
2017-02-22 23:43:28 -0500

    ES2017 Cover

---
+++ frontend/src/views/resume.ts
@@ -0,0 +1,93 @@
+export default {
+  social: [
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/alainxyz'
+    },{
+      name: 'github',
+      url: 'https://github.com/alaingalvan'
+    }, {
+      name: 'linkedin',
+      url: 'https://linkedin.com/in/alaingalvan'
+    }, {
+      name: 'artstation',
+      url: 'https://www.artstation.com/artist/alaingalvan'
+    }, {
+      name: 'deviantart',
+      url: 'https:///alaingalvan.deviantart.com'
+    }, {
+      name: 'soundcloud',
+      url: 'http://soundcloud.com/alaingalvan'
+    }, {
+      name: 'codepen',
+      url: 'https://codepen.io/alaingalvan'
+    }, {
+      name: 'youtube',
+      url: 'https://www.youtube.com/user/AGCreate'
+    }, {
+      name: 'tumblr',
+      url: 'http://alaingalvan.tumblr.com/'
+    }
+  ],
+  experience: [{
+    position: 'Graduate Graphics Research Assistant',
+    company: 'Florida International University',
+    cover: '',
+    startTime: new Date('06/06/2013'),
+    endTime: new Date('06/06/2013'),
+    description: "I'm responsible for creating an Unreal Engine 4 research project adding a procedurally generated skybox system and Windows 8 multitouch support. This project was further developed into a gesture elicitation experiment system, as well as a paper published to a conference. In addition, I serve as a Teacher's Assistant for Web Application Development creating assignments/exams/grading, and give workshops on Khronos Standards (OpenGL, OpenCL, WebGL, WebCL), Unreal Engine 4, HTML5 and Node.js."
+  }],
+  awards: [
+    {
+      award: 'MLH SudoHacks 1st Place',
+      company: 'Stetson University',
+      date: new Date('04/01/2016'),
+      place: 1,
+      description: 'Me and my team won 1st place for making an RESTful api abstraction layer on top of PeopleSoft with the addition of analytics and a modern frontend.'
+    },
+    {
+      award: 'MLH UHack 3rd Place',
+      company: 'University of Miami',
+      date: new Date('04/01/2016'),
+      place: 3,
+      description: 'Me and my team won 3rd place for a web application that trains beginners to read music by plugging in their piano and playing in real time. '
+    },
+    {
+      award: 'Codepen.io Frontpage',
+      company: 'CodePen.io',
+      date: new Date('03/01/2016'),
+      place: 0,
+      description: 'Front paged for a React Component that would later become the navigation component in my website.'
+    },
+    {
+      award: 'Newgrounds Frontpage',
+      company: 'Newgrounds',
+      date: new Date('05/01/2014'),
+      place: 0,
+      description: 'Awarded for a concept screenshot of a pixel art RPG. Received over 16K views.'
+    }
+  ],
+  volunteer: [
+    {
+      position: 'Worship Leader',
+      company: 'Prinston Church of the Nazarene',
+      startTime: new Date('09/01/2014'),
+      endTime: null,
+      description: 'I serve as a worshiper playing electric and acoustic guitar, in addition to serving as the backup pianist or bassist. '
+    },
+    {
+      position: 'Speaker',
+      company: 'Miami Game Developer Meetup',
+      startTime: new Date('09/01/2015'),
+      endTime: null,
+      description: "I've taught in numerous meetups with topics related to game development, including Zbrush Modeling, Pixel Art Design, Game Maker Programming, and Making Art for Games."
+    },
+    {
+      position: 'Worship Leader',
+      company: 'The Way Church of Miami',
+      startTime: new Date('07/01/2010'),
+      endTime: new Date('11/01/2013'),
+      description: "I served as a worship leader, performing every service on Piano, Guitar, or Violin. In addition, I educated new worshipers, and served as a technical director for the church's computer systems."
+    }
+  ]
+}
