6aea52b786a8ef0c45dd1b5d70842eb6d01ca42f
Alain Galvan <hi@alain.xyz>
2017-04-30 21:37:12 -0400

    Cleaned up server setup

--- packages/daemon/src/main.rs
+++ packages/daemon/src/main.rs
@@ -7,7 +7,7 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use nickel::{Nickel, HttpRouter, Request, Response};
+use nickel::{Nickel, HttpRouter, Request, Response, MediaType};
 use std::process::Command;
 
 #[derive(Serialize, Deserialize)]
@@ -15,27 +15,28 @@ struct Secret {
     secret: String,
 }
 
-#[derive(RustcDecodable, RustcEncodable)]
-struct APIRequest {}
-
 fn main() {
 
     let mut server = Nickel::new();
 
     let config: Secret = serde_json::from_str(include_str!("secret.json")).unwrap();
 
-    server.post("/",  middleware! { | req, res | { 
+    server.post("/",  middleware! { | req, mut res | { 
       let header = req.origin.headers.get_raw("X-GitHub-Delivery").unwrap().get(0).unwrap().to_vec();
 
       let github_header = String::from_utf8(header).unwrap();
 
-      if github_header == config.secret {
-          Command::new("cd ../ && cd portfolio && npm start")
-                      .output()
-            .expect("failed to execute process");
-      }
+      let data = req.param("ref").unwrap();
 
-      format!("")
+      if github_header == config.secret {
+          if data == "refs/heads/master" {
+            Command::new("git pull && cd ../ && cd portfolio && npm start")
+                        .output()
+                .expect("failed to execute process");
+          }
+      }
+      res.set(MediaType::Json);
+      format!("{{}}")
       }
     });
 
